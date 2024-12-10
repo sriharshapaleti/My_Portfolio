6542494f7ca9d91a1300df60e85b1abaa4b676fb
sriharshapaleti <140475614+sriharshapaleti@users.noreply.github.com>
2024-12-10 07:04:10 +0000

    minor modifications

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -27,7 +27,7 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download Resume
           </Button>
         </Row>
 
@@ -45,7 +45,7 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download Resume
           </Button>
         </Row>
       </Container>
